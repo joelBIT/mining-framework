152dca9906d9d378e8d80a940dc4b245d661dd7e
joelBIT <joel.rollny@gmail.com>
2018-03-19 17:10:32 +0100

    Multiple dataset names can be input in order to allow performing analysis on multiple datasets at a time

--- src/main/java/joelbits/modules/analysis/AnalysisModule.java
+++ src/main/java/joelbits/modules/analysis/AnalysisModule.java
@@ -3,6 +3,7 @@ package joelbits.modules.analysis;
 import joelbits.modules.analysis.plugins.AnalysisService;
 import joelbits.modules.analysis.plugins.spi.Analysis;
 import joelbits.utils.PathUtil;
+import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
@@ -17,7 +18,10 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Minimum 3 input parameters;  --analysisPlugin --analysis --outputFileName --dataset(s)
@@ -43,15 +47,54 @@ public class AnalysisModule extends Configured implements Tool {
 
     @Override
     public int run(String[] args) throws Exception {
+        List<String> datasets = new ArrayList<>();
+        checkArguments(args, datasets);
+
         Configuration configuration = this.getConf();
+        Job job = Job.getInstance(configuration, "Analysis Job");
+        loadAnalysisPlugin(args, job);
+        setDefaultSettings(job);
+        loadDatasets(datasets, job);
+        FileOutputFormat.setOutputPath(job, new Path(PathUtil.jarPath() + OUTPUT_JOB_DIRECTORY));
+
+        int completionStatus = job.waitForCompletion(true) ? 0 : 1;
+        if (completionStatus == 0) {
+            createOutput(args[2], configuration);
+        }
+
+        return completionStatus;
+    }
+
+    private void createOutput(String arg, Configuration configuration) {
+        String inputJobDirectory = PathUtil.jarPath() + OUTPUT_JOB_DIRECTORY;
+        String outputFile = PathUtil.jarPath() + arg;
+        try {
+            FileUtil.copyMerge(FileSystem.get(configuration), new Path(inputJobDirectory), FileSystem.get(configuration), new Path(outputFile), true, configuration, null);
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+
+    private void checkArguments(String[] args, List<String> datasets) {
         if (args.length < 3) {
             System.err.println("At least three parameters are expected");
             System.exit(2);
+        } else if (args.length > 3) {
+            for (int i = 3; i < args.length; i++) {
+                datasets.add(PathUtil.jarPath() + args[i] + File.separator);
+            }
         }
+    }
 
-        Job job = Job.getInstance(configuration, "Analysis Job");
+    private void setDefaultSettings(Job job) {
         job.setJarByClass(AnalysisModule.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        job.setInputFormatClass(SequenceFileInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+    }
 
+    private void loadAnalysisPlugin(String[] args, Job job) {
         try {
             Analysis analysisPlugin = AnalysisService.getInstance().getAnalysisPlugin(args[0]);
             job.setMapperClass(analysisPlugin.mapper(args[1]));
@@ -60,29 +103,17 @@ public class AnalysisModule extends Configured implements Tool {
             System.err.println("Could not find mapper/reducer for " + args[1]);
             System.exit(2);
         }
+    }
 
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
-
-        FileInputFormat.addInputPath(job, new Path(PathUtil.projectSequenceFile()));
-        //FileInputFormat.addInputPaths(job, "commaSeparatedPaths");  // use this to take input dataset to allow for replication (combining several dataset for a specific analysis)
-        job.setInputFormatClass(SequenceFileInputFormat.class);
-
-        FileOutputFormat.setOutputPath(job, new Path(PathUtil.jarPath() + OUTPUT_JOB_DIRECTORY));
-        job.setOutputFormatClass(TextOutputFormat.class);
-
-        int completionStatus = job.waitForCompletion(true) ? 0 : 1;
-
-        if (completionStatus == 0) {
-            String inputJobDirectory = PathUtil.jarPath() + OUTPUT_JOB_DIRECTORY;
-            String outputFile = PathUtil.jarPath() + args[2];
-            try {
-                FileUtil.copyMerge(FileSystem.get(configuration), new Path(inputJobDirectory), FileSystem.get(configuration), new Path(outputFile), true, configuration, null);
-            } catch (IOException e) {
-                System.err.println(e);
+    private void loadDatasets(List<String> datasets, Job job) {
+        try {
+            if (datasets.isEmpty()) {
+                FileInputFormat.addInputPath(job, new Path(PathUtil.projectSequenceFile()));
+            } else {
+                FileInputFormat.addInputPaths(job, StringUtils.join(datasets, ","));
             }
+        } catch (IOException e) {
+            System.err.println("Could not find one or more of the datasets");
         }
-
-        return completionStatus;
     }
 }
